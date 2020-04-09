bf7b589676df8d6cab05b67a1b902fa918f6dc0c
david <davidliang929@gmail.com>
2020-04-08 22:11:34 -0400

    worked on passport

--- app.js
+++ app.js
@@ -42,6 +42,30 @@ app.get('/login', (req, res) => {
 });
 
 
+const passport = require('passport')
+  , LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function(err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
+
+app.post('/login',
+  passport.authenticate('local', { successRedirect: '/',
+                                   failureRedirect: '/login',
+                                   failureFlash: true })
+);
+
 // app.post('/login',
 //   passport.authenticate('local'),
 //   function(req, res) {
