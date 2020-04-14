dce1232e22cd252a46b85bcb7b867a4e3a21087b
david <davidliang929@gmail.com>
2020-04-13 22:10:54 -0400

    added login/logout routes, passport

--- app.js
+++ app.js
@@ -3,18 +3,29 @@ require('./db');
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const passport = require('passport');
+const bcrypt = require('bcrypt');
+const LocalStrategy = require('passport-local').Strategy;
 
 const app = express();
 const list = [];
 
+//temporary user store
+const users = [];
+
 // enable sessions
 const session = require('express-session');
 const sessionOptions = {
-    secret: 'secret cookie thang (store this elsewhere!)',
+    secret: 'superdupersecret',
     resave: true,
       saveUninitialized: true
 };
 app.use(session(sessionOptions));
+
+
+//passport setup
+app.use(passport.initialize());
+app.use(passport.session());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -27,6 +38,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
+
+////////////////////////
+/////Route Handling/////
+////////////////////////
+
 app.get('/', (req, res) => {
   res.render('home', {list : list});
 });
@@ -37,34 +53,89 @@ app.post('/', (req,res) => {
   res.redirect('/');
 });
 
+
+
+
+//Login, Register
 app.get('/login', (req, res) => {
   res.render('login');
 });
 
+app.post('/login', passport.authenticate('local', {
+  failureRedirect: '/login',
+  successRedirect: '/'
+}));;
 
-const passport = require('passport')
-  , LocalStrategy = require('passport-local').Strategy;
+app.get('/register', (req, res) => {
+  res.render('register');
+})
+
+app.post('/register', (req,res) => {
+})
+
+
+passport.serializeUser((user, done) => {
+  done(null, user.username);
+});
+
+passport.deserializeUser((username, done) => {
+  done(null, {username: username});
+}); 
+
 
 passport.use(new LocalStrategy(
-  function(username, password, done) {
-    User.findOne({ username: username }, function(err, user) {
-      if (err) { return done(err); }
-      if (!user) {
-        return done(null, false, { message: 'Incorrect username.' });
+  (username, password, done) => {
+      if(username === 'test@gmail.com' && password === '1234') {
+          return done(null, {username: 'test@gmail.com'});
+      } else {
+          return done(null, false);
       }
-      if (!user.validPassword(password)) {
-        return done(null, false, { message: 'Incorrect password.' });
-      }
-      return done(null, user);
-    });
-  }
+   }
 ));
 
-app.post('/login',
-  passport.authenticate('local', { successRedirect: '/',
-                                   failureRedirect: '/login',
-                                   failureFlash: true })
-);
+function isLoggedIn(req, res, next){
+  if(req.isAuthenticated()){
+    return next;
+  }
+  else{
+    return res.redirect('/login');
+  }
+}
+
+
+// app.configure(function() {
+//   app.use(express.static('public'));
+//   app.use(express.cookieParser());
+//   app.use(express.bodyParser());
+//   app.use(express.session({ secret: 'keyboard cat' }));
+//   app.use(passport.initialize());
+//   app.use(passport.session());
+//   app.use(app.router);
+// });
+
+// const passport = require('passport')
+//   , LocalStrategy = require('passport-local').Strategy;
+
+// passport.use(new LocalStrategy(
+//   function(username, password, done) {
+//     User.findOne({ username: username }, function(err, user) {
+//       if (err) { return done(err); }
+//       if (!user) {
+//         return done(null, false, { message: 'Incorrect username.' });
+//       }
+//       if (!user.validPassword(password)) {
+//         return done(null, false, { message: 'Incorrect password.' });
+//       }
+//       return done(null, user);
+//     });
+//   }
+// ));
+
+// app.post('/login',
+//   passport.authenticate('local', { successRedirect: '/',
+//                                    failureRedirect: '/login',
+//                                    failureFlash: true })
+// );
 
 // app.post('/login',
 //   passport.authenticate('local'),
